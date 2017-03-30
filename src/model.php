7d5168607c1151b969964fce85fdb2b2b8124dfb
15marosm <mmaroswork@gmail.com>
2017-03-30 11:08:35 +0200

    Prof Help / update

--- src/model.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function getSejours(){
-	$bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
-	$sejours = $bdd->query('select * from sejour order by sejno');
-	return $sejours;
-}
-
-?>
